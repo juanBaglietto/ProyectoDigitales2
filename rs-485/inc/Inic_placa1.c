8b7dd7ffe450b5576ae961412dde544af95fb8ca
juan <juan.baglietto.01@gmail.com>
2015-12-01 00:12:00 -0300

    rs 485 sobre rtos

--- rs-485/inc/Inic_placa1.c
+++ rs-485/inc/Inic_placa1.c
@@ -14,7 +14,7 @@
 void kitInic(void)
 {
  InicGPIOs();
- InicTimers();
+ //InicTimers();
  InicUart1();
 
 
@@ -25,33 +25,33 @@ void InicGPIOs ( void )
 	Chip_GPIO_WritePortBit(LPC_GPIO,LEDXpresso,1);
 }
 
-void InicTimers(void){
-
-	uint32_t timerFreq;
-
-	//inicializo los timers para la maquina de estado
-		Chip_TIMER_Init(LPC_TIMER2);
-
-	//tomo la frecuencia del clok
-		timerFreq = Chip_Clock_GetSystemClockRate();
-
-
-	//confinfiguro el timer 2 y el match 1
-
-		Chip_TIMER_Reset(LPC_TIMER2);
-		Chip_TIMER_MatchEnableInt(LPC_TIMER2, 1);
-		Chip_TIMER_SetMatch(LPC_TIMER2, 1, 250);
-		Chip_TIMER_ResetOnMatchEnable(LPC_TIMER2, 1);
-
-		Chip_TIMER_Enable(LPC_TIMER2);
-
-		//habilito interrupciones
-
-		NVIC_ClearPendingIRQ(TIMER2_IRQn);
-		NVIC_EnableIRQ(TIMER2_IRQn);
-
-
-}
+//void InicTimers(void){
+//
+//	uint32_t timerFreq;
+//
+//	//inicializo los timers para la maquina de estado
+//		Chip_TIMER_Init(LPC_TIMER2);
+//
+//	//tomo la frecuencia del clok
+//		timerFreq = Chip_Clock_GetSystemClockRate();
+//
+//
+//	//confinfiguro el timer 2 y el match 1
+//
+//		Chip_TIMER_Reset(LPC_TIMER2);
+//		Chip_TIMER_MatchEnableInt(LPC_TIMER2, 1);
+//		Chip_TIMER_SetMatch(LPC_TIMER2, 1, 250);
+//		Chip_TIMER_ResetOnMatchEnable(LPC_TIMER2, 1);
+//
+//		Chip_TIMER_Enable(LPC_TIMER2);
+//
+//		//habilito interrupciones
+//
+//		NVIC_ClearPendingIRQ(TIMER2_IRQn);
+//		NVIC_EnableIRQ(TIMER2_IRQn);
+//
+//
+//}
 
 void InicUart1(void){
 
